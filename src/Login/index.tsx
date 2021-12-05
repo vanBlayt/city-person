175cbecb027626b3f149ba71e7c8c429959ec311
van <1075236902@qq.com>
2021-12-05 22:26:32 +0800

    feat: login UI

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
 import './index.scss';
+import { TextField, Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
+
+
 function Login() {
+  // 初始化form表单
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function loginSubmit(e: any) {
+    const { username, password } = e;
+    console.log(username, password);
+  }
+
   return (
-    <div id="login">
-      <h1>Login</h1>
-      <form method="post">
-        <input className='login-input' type="text" required placeholder="用户名" name="u"></input>
-        <input className='login-input' type="password" required placeholder="密码" name="p"></input>
-        <button className="but" type="submit">登录</button>
-      </form>
+    <div className='login-wrapper'>
+      <div className="login">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit(loginSubmit)}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="username"
+            label="用户名"
+            autoComplete="username"
+            autoFocus
+            {...register('username')}
+            required
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="密码"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            {...register('password')}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            登录
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
